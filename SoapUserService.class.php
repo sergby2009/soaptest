f7282d04f0bc5835ef7d60e185bf0723b2d3e554
S.Dugin <miror99@mail.ru>
2019-11-27 17:26:52 +0400

    Apdate 20191127

--- SoapUserService.class.php
+++ SoapUserService.class.php
@@ -7,17 +7,17 @@ class User{
     public $pasport;
 }
 class UserList{
-    public $users;
+    public $userList;
 }
 
-class sendRequest{
-    public $userlist;
+class SendRequest{
+    public $sendRequest;
 }
 
 
 class SoapUserService
 {
-    private static $userArray = array(
+    private $userArray = array(
         "1" => array(
             "FirstName" => "Сергей",
             "LastName" => "Петров",
@@ -32,12 +32,32 @@ class SoapUserService
             "pasport"=> "8735-987654"),
     );
 
-    public function sendUser($userlist){
-        if (isset($userlist)){
-            foreach ($userlist->$users as $user) {
-                $this->userArray[]
+    public function sendUser(SendRequest $data){
+        if (!is_null($data)){
+            foreach ($data->users as $user) {
+                $this->userArray[] = $user;
+            }
+
             }
 
         }
     }
 }
+//
+$req = new SendRequest();
+
+$req->sendRequest = new UserList();
+
+$user = new User();
+$user->firstname = "Иван";
+$user->lastname = "Грозный";
+$user->bday = "01-01-1502";
+$user->pasport = "0000-000000";
+$user->tel = "000-000-00-00";
+
+$req->sendRequest->userList[]  = $user;
+$req->sendRequest->userList[]  = $user;
+$req->sendRequest->userList[]  = $user;
+
+$srv = new SoapUserService();
+$srv->sendUser($req);
